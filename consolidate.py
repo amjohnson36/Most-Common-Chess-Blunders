f26c2887962cd769618cff8649a257a6edc14ded
Alex Johnson <amjohnson36@crimson.ua.edu>
2020-01-07 16:30:57 -0600

    Fixed consolidate.py

--- consolidate.py
+++ consolidate.py
@@ -2,13 +2,6 @@ import os
 
 data = {}
 count = 0
-"""
-with open('Consolidated.output', 'r') as infile:
-    for line in infile.readlines():
-        l = line.split(',')
-        blunder = l[0] + ',' + l[1]
-        data[blunder] = int(l[2])
-"""
 
 for filename in os.listdir("results/"):
     with open('results/{0}'.format(filename), 'r') as infile:
@@ -16,6 +9,7 @@ for filename in os.listdir("results/"):
             l = line.split(',')
             
             if l[2] == "1\n":
+            # skip blunders that were only played once to save memory
                 continue
             
             blunder = l[0] + ',' + l[1]
@@ -30,10 +24,7 @@ for filename in os.listdir("results/"):
     print(filename + " done")
 
 
-
-
-with open('test', 'w') as outfile:
+with open('Consolidated.output', 'w') as outfile:
         sorted_data = sorted(data, key=data.get, reverse=True)
         for r in sorted_data:
             outfile.write('{0},{1}\n'.format(r, data[r]))
-
